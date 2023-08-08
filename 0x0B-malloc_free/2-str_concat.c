6c84833e08bb47a2084fbe7d3f989ea5f3473223
OkoyoTrevor <trevorokoyo@gmail.com>
2023-08-08 09:58:48 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,22 +10,15 @@
 
 char *str_concat(char *s1, char *s2)
 {
-		
 	char *conct;
 		
 	int i, ci;
 		
-
-		
 	if (s1 == NULL)
-		
 		s1 = "";
 		
 	if (s2 == NULL)
-		
 		s2 = "";
-		
-
 		
 		i = ci = 0;
 		
@@ -33,44 +26,25 @@ char *str_concat(char *s1, char *s2)
                 i++;
 		
 	while (s2[ci] != '\0')
-		
 		ci++;
 		
 	conct = malloc(sizeof(char) * (i + ci + 1));
 		
-
-		
 	if (conct == NULL)
-		
 		return (NULL);
 		
 	i = ci = 0;
 		
-	while (s1[i] != '\0')
-		
+	while (s1[i] != '\0')	
 	{
-		
 		conct[i] = s1[i];
-		
-		i++;
-		
+		i++;	
 	}
-		
-
-		
-	while (s2[ci] != '\0')
-		
+	while (s2[ci] != '\0')	
 	{
-		
 		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+		i++, ci++;	
 	}
-		
 	conct[i] = '\0';
-		
-	return (conct);
-		
+	return (conct);		
 }
-
