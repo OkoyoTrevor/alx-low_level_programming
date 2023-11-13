5dfc2e33efaeebe49ba39e14eb4b67bdae62fc77
OkoyoTrevor <trevorokoyo@gmail.com>
2023-11-13 02:46:23 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Return: 0
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		if ((a != 2 && a != 4) && a < 10)
+		_putchar("%d", a);
+		a++;
+	}
+	_putchar("\n");
+	return (0);
+}
